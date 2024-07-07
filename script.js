220e0b85bd1a5ed481b118cfc04002116502c5c2
Juris Raudulis <juris.raudulis@outlook.com>
2024-07-07 19:13:03 +0100

    Uploading progress

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-const showCoordinates = document.getElementById('coordinates');
+const h2 = document.getElementById('coordinates');
 
-async function trackSpaceStation(){
+async function trackSpaceStation() {
 
  const response = await fetch('http://api.open-notify.org/iss-now.json');
  const getLocation = await response.json();
  console.log(getLocation)
+ return getLocation;
 }
 
- trackSpaceStation();
+function getCoordinates (){
+  const latitude = getLocation.iss_position.latitude;
+  const longitude = getLocation.iss_position.longitude;
+}
+
+const updateDisplay = () => {
+ h2.textContent = getCoordinates()
+}
+
+trackSpaceStation();
+updateDisplay();
