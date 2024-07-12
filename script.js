a067c4f454225e7bcb7ab2f8f031a3b9bd165f25
Juris Raudulis <juris.raudulis@outlook.com>
2024-07-12 17:35:06 +0100

    Uploading progress made for the project

--- script.js
+++ script.js
@@ -12,28 +12,31 @@ function getCoordinates(coordinates) {
  return { latitude, longitude};
 }
 
-async function updateDisplay() {
- try{
- const locationData = await fetchData();
- const cor = getCoordinates(locationData);
- h2.textContent = `latitude ${cor.latitude} Longitude ${cor.longitude}`;
- }catch(err){
-  console.log('Oops daisy', err)
- }
-}
 
-const map = L.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map').setView([0,0], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 2,
+    maxZoom: 3,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-const circle = L.circle([51.508, -0.11], {
+const circle = L.circle([0,0], {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5,
     radius: 500
 }).addTo(map);
 
-updateDisplay();
+setInterval(async function() {
+ try{
+ const locationData = await fetchData();
+ const cor = getCoordinates(locationData);
+  h2.textContent = `Latitude ${cor.latitude} Longitude ${cor.longitude}`;
+
+circle.setLatLng([cor.latitude, cor.longitude]);
+map.setView([cor.latitude, cor.longitude], 3)
+
+ }catch(err){
+  console.log('Oops daisy', err)
+ }
+}, 5000);
