6a0c09a3642c32a163a090f7ace1ccf9570a1567
Juris Raudulis <juris.raudulis@outlook.com>
2024-07-22 17:50:29 +0100

    Fixed API issue by changing it to source with HTTPS protocol

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const h2 = document.getElementById('coordinates');
 
 const fetchData = async function() {
- const response = await fetch(`http://api.open-notify.org/iss-now.json?timestamp=${new Date().getTime()}`);
+ const response = await fetch(`https://api.wheretheiss.at/v1/satellites/25544`);
  const getLocation = await response.json();
  return getLocation;
 }
 
 function getCoordinates(coordinates) {
- const latitude = coordinates.iss_position.latitude;
- const longitude = coordinates.iss_position.longitude;
+ const latitude = coordinates.latitude;
+ const longitude = coordinates.longitude;
  return { latitude, longitude};
 }
 
@@ -16,26 +16,25 @@ function getCoordinates(coordinates) {
 const map = L.map('map').setView([0,0], 2);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 3,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+   maxZoom: 3,
+   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
 const circle = L.circle([0,0], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
+   color: 'red',
+   fillColor: '#f03',
+   fillOpacity: 0.5,
+   radius: 500
 }).addTo(map);
 
 setInterval(async function() {
  try{
- const locationData = await fetchData();
- const cor = getCoordinates(locationData);
- console.log(cor);
+  const locationData = await fetchData();
+  const cor = getCoordinates(locationData);
   h2.textContent = `Latitude ${cor.latitude} Longitude ${cor.longitude}`;
 
-circle.setLatLng([cor.latitude, cor.longitude]);
-map.panTo([cor.latitude, cor.longitude])
+  circle.setLatLng([cor.latitude, cor.longitude]);
+  map.panTo([cor.latitude, cor.longitude])
 
  }catch(err){
   console.log('Oops daisy', err)
